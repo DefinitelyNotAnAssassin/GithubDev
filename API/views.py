b013825c5cd30cc543bd2397e3b0e09ecd0a2ae5
Winmari Manzano <46719110+DefinitelyNotAnAssassin@users.noreply.github.com>
2024-11-20 19:02:14 +0800

    Update views.py

--- API/views.py
+++ API/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render 
-from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
+from django.http import HttpResponse, JsonResponse
 from Models.models import UserRecord     
 import requests
 import json
 
 def get_repo_info(username): 
     headers = {
-        'Authorization': 'token '
+        'Authorization': 'token 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'
     }
     
-    response = requests.get(f'https://api.github.com/users/{username}/repos?per_page=100', headers=headers)
-    return response.json()
+    try:
+        response = requests.get(f'https://api.github.com/users/{username}/repos?per_page=100', headers=headers)
+        return response.json()
+    except Exception as e:
+        print(e)
+        return []
 
 
 def get_repo_loc(username, repo_name):
@@ -21,45 +25,46 @@ def get_repo_loc(username, repo_name):
     except json.decoder.JSONDecodeError: 
         print(response.text)
         return {}
+    except Exception as e:
+        print(e)
+        return {}
 
 
 def getLinesOfCode(request, username):
-    
-    
-    def stream_response():
-        user_record = UserRecord.objects.filter(username=username).first()
-        if user_record:
-            yield "event: message\ndata: User already exists\n\n"
-            yield f"event: message\ndata: Total lines of code: {user_record.lines_of_code}\n\n"
-            yield f"event: message\ndata: Lines of code per language: {json.dumps(user_record.lines_of_code_per_language)}\n\n"
-            yield "event: message\ndata: Success\n\n"
-            return
+    user_record = UserRecord.objects.filter(username=username).first()
+    if user_record:
+        return JsonResponse({
+            "message": "User already exists",
+            "total_lines_of_code": user_record.lines_of_code,
+            "lines_of_code_per_language": user_record.lines_of_code_per_language,
+            "status": "Success"
+        })
 
-        yield "event: message\ndata: User does not exist, processing...\n\n"
-        repositories = get_repo_info(username)
-        lines_of_code = 0
-        lines_of_code_per_language = {}
+    repositories = get_repo_info(username)
+    lines_of_code = 0
+    lines_of_code_per_language = {}
 
-        for repository in repositories:
-            yield f"event: message\ndata: Processing repository: {repository['name']}\n\n"
-            loc = get_repo_loc(username, repository['name'])
-            lines_of_code += loc.get('loc', 0)
+    for repository in repositories:
+        loc = get_repo_loc(username, repository['name'])
+        lines_of_code += loc.get('loc', 0)
 
-            for lang, count in loc.get('locByLangs', {}).items():
-                if lang in lines_of_code_per_language:
-                    lines_of_code_per_language[lang] += count
-                else:
-                    lines_of_code_per_language[lang] = count
+        for lang, count in loc.get('locByLangs', {}).items():
+            if lang in lines_of_code_per_language:
+                lines_of_code_per_language[lang] += count
+            else:
+                lines_of_code_per_language[lang] = count
 
-        user_record = UserRecord(
-            username=username,
-            lines_of_code=lines_of_code,
-            lines_of_code_per_language=lines_of_code_per_language,
-            repositories=json.dumps(repositories)
-        )
-        user_record.save()
-        yield f"event: message\ndata: Total lines of code: {user_record.lines_of_code}\n\n"
-        yield f"event: message\ndata: Lines of code per language: {json.dumps(user_record.lines_of_code_per_language)}\n\n"
-        yield "event: message\ndata: Success\n\n"
+    user_record = UserRecord(
+        username=username,
+        lines_of_code=lines_of_code,
+        lines_of_code_per_language=lines_of_code_per_language,
+        repositories=json.dumps(repositories)
+    )
+    user_record.save()
 
-    return StreamingHttpResponse(stream_response(), content_type='text/event-stream')
+    return JsonResponse({
+        "total_lines_of_code": user_record.lines_of_code,
+        "lines_of_code_per_language": json.dumps(user_record.lines_of_code_per_language),
+        "status": "Success"
+    })
+
