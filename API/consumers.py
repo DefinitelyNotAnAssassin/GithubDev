676a1f81bf1e02ca64f71f261794ff122dc3154f
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-11-25 00:40:54 +0800

    Working ASYNC

--- API/consumers.py
+++ API/consumers.py
@@ -1,49 +1,55 @@
-# API/consumers.py
-
 import json
 import logging
 import os
-import asyncio
-import aiohttp
-from channels.generic.websocket import WebsocketConsumer
+import requests
+from threading import Thread, Event
+from channels.generic.websocket import AsyncWebsocketConsumer
 from API.utils.LinesOfCode import RepoAnalyzer
 from Models.models import UserRecord
 from API.constants.ExtensionFilters import default_ignore_extensions, default_ignore_dirs
+import asyncio
+import aiohttp
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-GITHUB_API_URL = 'https://api.github.com/users/{username}/repos?per_page=2'
+GITHUB_API_URL = 'https://api.github.com/users/{username}/repos?per_page=100'
 MAX_REPOSITORY_SIZE = 150000  # kilobytes
+class LinesOfCodeConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.is_connected = True
+        await self.accept()
+        self.stop_event = asyncio.Event()
 
-class LinesOfCodeConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+    async def disconnect(self, close_code):
+        self.is_connected = False
+        self.stop_event.set()
+        print("Disconnected")
+        await self.close()
 
-    def disconnect(self, close_code):
-        self.close()
-        pass
-    
-    def receive(self, text_data):
+    async def receive(self, text_data):
         data = json.loads(text_data)
-        username = data['username']
-        ignore_dirs = set(data.get('ignore_dirs', default_ignore_dirs))
+
+
+        username = data.get('username')
+        ignore_dirs = set(data.get('ignore_dirs', default_ignore_extensions))
         ignore_extensions = set(data.get('ignore_extensions', default_ignore_extensions))
 
         try:
-           
-            repositories = self.get_repo_info(username)
+            repositories = await self.get_repo_info(username)
             total_repos = len(repositories)
             processed_repos = 0
             lines_of_code = 0
             lines_of_code_per_language = {}
 
             for repository in repositories:
+                if self.is_connected == False:
+                    return
+
                 processed_repos += 1
-                self.send_progress(repository, processed_repos, total_repos)
-                asyncio.sleep(0)  # Yield control to the event loop
+                await self.send_progress(repository, processed_repos, total_repos)
                 if repository['size'] > MAX_REPOSITORY_SIZE or repository['size'] == 0 or repository['fork']:
                     continue
 
-                loc = RepoAnalyzer(username, repository['name'], ignore_dirs, ignore_extensions).analyze()
+                loc = await asyncio.to_thread(RepoAnalyzer(username, repository['name'], ignore_dirs, ignore_extensions).analyze)
                 lines_of_code += loc.get('loc', 0)
                 for lang, count in loc.get('locByLangs', {}).items():
                     lines_of_code_per_language[lang] = lines_of_code_per_language.get(lang, 0) + count
@@ -54,19 +60,22 @@ class LinesOfCodeConsumer(WebsocketConsumer):
                 lines_of_code_per_language=lines_of_code_per_language,
                 repositories=json.dumps(repositories)
             )
-            self.send(text_data=json.dumps({'type': 'result', 'total_lines_of_code': user_record.lines_of_code, 'lines_of_code_per_language': lines_of_code_per_language}))
-            self.send(text_data=json.dumps({'type': 'complete'}))
+            user_record.save()
+            await self.send(text_data=json.dumps({'type': 'result', 'total_lines_of_code': user_record.lines_of_code, 'lines_of_code_per_language': lines_of_code_per_language}))
+            await self.send(text_data=json.dumps({'type': 'complete'}))
         except Exception as e:
             logging.error(f"Error processing user {username}: {e}")
-            return 
-        
-    def get_repo_info(self, username):
-        headers = {'Authorization': f'token {GITHUB_TOKEN}'}
-        with aiohttp.ClientSession() as session:
-            with session.get(GITHUB_API_URL.format(username=username), headers=headers) as response:
-                return response.json()
+            await self.send(text_data=json.dumps({'type': 'error', 'message': str(e)}))
 
-    def send_progress(self, repository, processed_repos, total_repos):
+    async def get_repo_info(self, username):
+        headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(GITHUB_API_URL.format(username=username), headers=headers) as response:
+                return await response.json()
+
+
+
+    async def send_progress(self, repository, processed_repos, total_repos):
         message = {
             'type': 'progress',
             'repo': repository['name'],
@@ -74,4 +83,4 @@ class LinesOfCodeConsumer(WebsocketConsumer):
             'totalRepos': total_repos
         }
         logging.debug(f"Sending message: {message}")
-        self.send(text_data=json.dumps(message))
+        await self.send(text_data=json.dumps(message))
--- API/utils/LinesOfCode.py
+++ API/utils/LinesOfCode.py
@@ -10,7 +10,6 @@ import tempfile
 from collections import Counter
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class RepoAnalyzer:
     def __init__(self, username, repo_name, ignore_dirs=None, ignore_extensions=None):
@@ -22,7 +21,6 @@ class RepoAnalyzer:
         self.clone_base_dir = tempfile.mkdtemp()
         self.clone_dir = self.clone_base_dir  # Will be updated after extraction
         self.directory_counter = Counter()
-        logging.info(f"Initialized RepoAnalyzer for {username}/{repo_name}")
 
     def get_default_branch(self):
         api_url = f"https://api.github.com/repos/{self.username}/{self.repo_name}"
@@ -33,9 +31,7 @@ class RepoAnalyzer:
         response = requests.get(api_url, headers=headers)
         if response.status_code == 200:
             repo_info = response.json()
-            default_branch = repo_info.get('default_branch', 'master')
-            logging.info(f"Default branch for {self.username}/{self.repo_name} is {default_branch}")
-            return default_branch
+            return repo_info.get('default_branch', 'master')
         else:
             error_message = f"Failed to get repository info: {response.status_code}"
             logging.error(error_message)
@@ -54,14 +50,12 @@ class RepoAnalyzer:
         if token:
             headers['Authorization'] = f'token {token}'
 
-        logging.info(f"Downloading repository {self.username}/{self.repo_name} from {repo_url}")
         response = requests.get(repo_url, headers=headers, stream=True)
         if response.status_code == 200:
             with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
                 zip_ref.extractall(self.clone_base_dir)
             extracted_folder_name = f"{self.repo_name}-{default_branch}"
             self.clone_dir = os.path.join(self.clone_base_dir, extracted_folder_name)
-            logging.info(f"Extracted repository to {self.clone_dir}")
         else:
             error_message = f"Failed to download repository: {response.status_code}"
             logging.error(error_message)
@@ -83,10 +77,8 @@ class RepoAnalyzer:
                         comment_lines += 1
                     else:
                         lines_of_code += 1
-            logging.info(f"Processed file {file_path}")
         except Exception as e:
             logging.error(f"Error processing file {file_path}: {e}")
-
         return lines_of_code, comment_lines, blank_lines
 
     def count_lines_of_code(self):
@@ -107,7 +99,6 @@ class RepoAnalyzer:
             self.directory_counter.update(dirs)
 
         if not files_to_process:
-            logging.info("No files to process in the repository.")
             return lines_of_code, comment_lines, blank_lines, lines_of_code_per_language
 
         def process_and_collect(args):
@@ -115,7 +106,6 @@ class RepoAnalyzer:
             loc, comments, blanks = self.process_file(file_path)
             return loc, comments, blanks, ext
 
-        logging.info(f"Starting to process {len(files_to_process)} files")
         with ThreadPoolExecutor(max_workers=os.cpu_count() * 5) as executor:
             results = executor.map(process_and_collect, files_to_process)
 
@@ -129,7 +119,6 @@ class RepoAnalyzer:
                 else:
                     lines_of_code_per_language[ext] = loc
 
-        logging.info(f"Finished processing files. Total LOC: {lines_of_code}, Comments: {comment_lines}, Blanks: {blank_lines}")
         return lines_of_code, comment_lines, blank_lines, lines_of_code_per_language
 
     def log_common_directories(self):
@@ -142,18 +131,17 @@ class RepoAnalyzer:
                 for directory, count in self.directory_counter.most_common():
                     log_file.write(f"{directory}: {count}\n")
                 log_file.write("\n")
-            logging.info(f"Logged common directories to {log_file_path}")
         except Exception as e:
             logging.error(f"Failed to log common directories: {e}")
 
     def analyze(self):
         try:
+            print("Analyzing repository... " + self.repo_name)
             self.download_and_extract_repo()
             loc, comments, blanks, loc_by_lang = self.count_lines_of_code()
             self.log_common_directories()
         finally:
             shutil.rmtree(self.clone_base_dir, ignore_errors=True)
-            logging.info(f"Cleaned up temporary directory {self.clone_base_dir}")
         return {
             'loc': loc,
             'comments': comments,
