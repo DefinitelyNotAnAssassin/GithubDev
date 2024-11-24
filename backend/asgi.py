2ea63b706ea67a8bb3b46aa0e8c8c13976cf3834
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-11-25 01:10:00 +0800

    DAPHNE CONFIGURATION

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,15 +1,20 @@
 import os
 import django
 from django.core.asgi import get_asgi_application
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+django_asgi_app = get_asgi_application()
+
+
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from API.consumers import LinesOfCodeConsumer
 
-# Set the default Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-django.setup()
-django_asgi_app = get_asgi_application()
+
+
 
 
 # Define the application
